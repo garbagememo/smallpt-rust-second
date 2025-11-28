9d22e5f62e525e2dc15c2e402efa1c0f0aeec38a
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-11-29 00:49:04 +0900

    オブジェクトが法線を返すように変更

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 mod raymod;
 use raymod::*;
+use std::sync::Arc;
 
 use rayon::prelude::*;
 
 use std::io::Write;
 
 fn radiance(r: &Ray, depth: u8,	scene: &Scene) -> Vec3 {
-    let ir=scene.intersect(r);
-    if ir.b==false {
-        return Vec3::zero();
-    }
-    let obj = &scene.objects[ir.id];
-    let x = r.o + r.d * ir.t;
-    let n = (x - obj.p).norm();
-    let nl = if n.dot(&r.d) < 0.0 { n } else { n * -1.0 };
-    let mut f = obj.m.value();
-    let p = f64::max(f.x,f64::max(f.y, f.z));
-    let depth = depth + 1;
-    if depth > 5 {
-        if depth < 127 && random() < p {
-            f = f * (1.0 / p);
-        } else {
-            return obj.m.emitted();
+    let hit_info = scene.intersect(r);
+    if let Some(info)=hit_info {
+        let nl = if info.n.dot(&r.d) < 0.0 { info.n } else { info.n * -1.0 };
+        let mut f = info.m.value();
+        let p = f64::max(f.x,f64::max(f.y, f.z));
+        let depth = depth + 1;
+        if depth > 5 {
+            if depth < 127 && random() < p {
+                f = f * (1.0 / p);
+            } else {
+                return info.m.emitted();
+            }
         }
+        let m_info = info.m.trace_ray(&r,info.n,nl,info.p);
+        info.m.emitted() + f.mult(&radiance(&m_info.r, depth,scene))*m_info.cpc
+    } else {
+        return BLACK;
     }
-    let m_info = obj.m.trace_ray(&r,n,nl,x);
-    obj.m.emitted() + f.mult(&radiance(&m_info.r, depth,scene))*m_info.cpc
+    
 }
 
 struct Camera{
@@ -59,32 +59,35 @@ impl Scene {
         let Cen1 = Vec3::new(75.0,25.0, 85.0);
         let Cen2 = Vec3::new(45.0,25.0, 30.0);
         let Cen3 = Vec3::new(15.0,25.0,-25.0);
-        self.add(Sphere::new(10000.0,  Cen+Vec3::new(0.0,0.0,-200.0)  ,
-                             Box::new(Diffuse::new(Vec3::new(0.6, 0.5, 0.7)*0.8, Vec3::new(0.7,0.9,1.0))),  )); // sky
+        self.add(Sphere::new(
+            10000.0,
+            Cen+Vec3::new(0.0,0.0,-200.0)  ,
+            Arc::new(Diffuse::new(Vec3::new(0.6, 0.5, 0.7)*0.8, Vec3::new(0.7,0.9,1.0))),  )); // sky
         self.add(Sphere::new(100000.0, Vec3::new(50.0, -100000.0, 0.0),
-                             Box::new(Diffuse::new(BLACK, Vec3::new(0.4,0.4,0.4))),)); // grnd
-        
-
+                             Arc::new(Diffuse::new(BLACK, Vec3::new(0.4,0.4,0.4))),)); // grnd
         self.add(Sphere::new(25.0,  Cen1 ,
-                             Box::new(Mirror::new(BLACK,Vec3::new(0.9,0.9,0.9))), ));// mirror
+                             Arc::new(Mirror::new(BLACK,Vec3::new(0.9,0.9,0.9))), ));// mirror
         self.add(Sphere::new(25.0,  Cen2 ,
-                             Box::new(Refract::new(BLACK,Vec3::new(0.95,0.95,0.95))), )); // glass
+                             Arc::new(Refract::new(BLACK,Vec3::new(0.95,0.95,0.95))), )); // glass
         self.add(Sphere::new(25.0,  Cen3 ,
-                             Box::new(Diffuse::new(BLACK,Vec3::new(1.0,0.6,0.6)*0.696)), ));    // 乱反射
+                             Arc::new(Diffuse::new(BLACK,Vec3::new(1.0,0.6,0.6)*0.696)), ));    // 乱反射
         for a in -11 .. 12 {
             for b in -11 .. 12 {
                 let RandomMatterial = random();
                 let Cen = Vec3::new( ((a as f64)+random())*25.0,5.0,((b as f64)+random() )*25.0);
                 if (Cen - Cen1) .length().sqrt()>(25.0*1.0) {
                     if RandomMatterial<0.8 {
-                        self.add(Sphere::new(5.0,Cen,
-                                             Box::new(Diffuse::new(BLACK,Vec3::new(random(),random(),random()))),));
+                        self.add(Sphere::new(
+                            5.0, Cen,
+                            Arc::new(Diffuse::new(BLACK,Vec3::new(random(),random(),random()))),));
                     } else if RandomMatterial <0.95 {
-                        self.add(Sphere::new(5.0,Cen,
-                                             Box::new(Mirror::new(BLACK,Vec3::new(random(),random(),random()))),));
+                        self.add(Sphere::new(
+                            5.0,Cen,
+                            Arc::new(Mirror::new(BLACK,Vec3::new(random(),random(),random()))),));
                     } else {
-                        self.add(Sphere::new(5.0,Cen,
-                                             Box::new(Refract::new(BLACK,Vec3::new(random(),random(),random()) )),));
+                        self.add(Sphere::new(
+                            5.0,Cen,
+                            Arc::new(Refract::new(BLACK,Vec3::new(random(),random(),random()) )),));
                     }
                 }
             }
